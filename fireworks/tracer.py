db0d91dbf9213af637ab1073f4898363146f417b
Thomas <thekileyenator@gmail.com>
2020-12-31 11:54:23 +0000

    Make tracer move like a rocket

--- fireworks/tracer.py
+++ fireworks/tracer.py
@@ -1,5 +1,6 @@
 import math
 import os
+from random import uniform, normalvariate
 
 import pyglet
 from lepton import Particle
@@ -20,7 +21,8 @@ speed = 1.0
 class Tracer:
     def __init__(self):
         self.time_to_explode = 1.0
-        self.starting_position = (0, -40, 0)
+        self.starting_position = (uniform(-44, 44), -40, normalvariate(0, 25))
+        self.starting_velocity = Vec3(uniform(-25, 25), normalvariate(100, 10), uniform(-10, 10))
         self.comet = StaticEmitter(
             rate=600,
             time_to_live=self.time_to_explode,
@@ -48,76 +50,11 @@ class Tracer:
             Kaboom(Vec3(self.comet.template.position.x, self.comet.template.position.y, self.comet.template.position.z))
             pyglet.clock.unschedule(self.tick)
         else:
-            vel = 80
-            self.comet.template.position.y += vel*dt
-            #self.comet.template.velocity.y = vel
-
-
-
-#############################################################################
-#
-# Copyright (c) 2008 by Casey Duncan and contributors
-# All Rights Reserved.
-#
-# This software is subject to the provisions of the MIT License
-# A copy of the license should accompany this distribution.
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-#
-#############################################################################
-"""Flying comet using textured billboard quads"""
-
-__version__ = '$Id$'
-
-import os
-import math
-from pyglet import image
-from pyglet.gl import *
-
-from lepton import Particle, ParticleGroup, default_system
-from lepton.renderer import BillboardRenderer
-from lepton.texturizer import SpriteTexturizer
-from lepton.emitter import StaticEmitter
-from lepton.controller import Gravity, Lifetime, Movement, Fader, ColorBlender
-
-# comet = StaticEmitter(
-#     rate=600,
-#     template=Particle(
-#         size=(2, 2, 0),
-#         color=(1, 1, 0),
-#     ),
-#     deviation=Particle(
-#         velocity=(0.7, 0.7, 0.7),
-#         up=(0, 0, math.pi),
-#         rotation=(0, 0, math.pi),
-#         color=(0.5, 0.5, 0.5))
-# )
-#
-# images = [image.load(os.path.join(os.path.dirname(__file__), 'resources/flare%s.png' % (i + 1)))
-#           for i in range(4)]
-# group = ParticleGroup(controllers=[comet],
-#                       renderer=BillboardRenderer(SpriteTexturizer.from_images(images)))
-#
-# arc_radius = 150
-# angle = math.pi * 0.7
-# speed = 1.0
-#
-#
-# def move_comet(td):
-#     global angle, arc_radius, speed
-#     comet.template.position = (
-#         -math.sin(angle) * arc_radius * 0.3,
-#         math.sin(angle * 0.7) * arc_radius * 0.03,
-#         -math.cos(angle) * arc_radius - arc_radius * 1.05)
-#     comet.template.velocity = (
-#         comet.template.position.x * 0.05 - comet.template.last_position.x,
-#         comet.template.position.y * 0.05 - comet.template.last_position.y,
-#         comet.template.position.z * 0.05 - comet.template.last_position.z)
-#     angle -= td * speed
+            self.starting_velocity.y -= 2
+            self.comet.template.position.x += self.starting_velocity.x * dt
+            self.comet.template.position.y += self.starting_velocity.y * dt
+            self.comet.template.position.z += self.starting_velocity.z * dt
 
 
 def run_tracer(dt):
-    # pyglet.clock.schedule_interval(move_comet, (1.0 / 30.0))
-    # move_comet(0)
     comet = Tracer()
